7421c25105e3bfa6b5e14b2e822ae85d061a29d5
amorgulis <>
2018-05-09 14:09:07 -0700

    Added getAthleteStats().

--- src/api.ts
+++ src/api.ts
@@ -3,9 +3,9 @@ import axios, { AxiosResponse } from 'axios';
 const STRAVA_API_URL = 'https://www.strava.com/api/v3';
 
 export class Strava {
-  constructor(private accessToken: string) { }
+  constructor(private accessToken: string) {}
 
-  private async doRequest<T>(path: string, params): Promise<T> {
+  private async doRequest<T>(path: string, params: object): Promise<T> {
     const url = `${STRAVA_API_URL}/${path}`;
 
     return axios
@@ -17,6 +17,22 @@ export class Strava {
         params: params
       })
       .then(res => res.data);
+  }
+
+  /**
+   * Returns the activity stats of an athlete
+   * @param id The identifier of the athlete.
+   * @param page Page number.
+   * @param perPage Number of items per page. Defaults to 30.
+   * @returns Returns the activity stats of an athlete.
+   */
+  async getAthleteStats(id: number, page?: number, perPage?: number) {
+    const path = `athletes/${id}/stats`;
+
+    return this.doRequest<ActivityStats>(path, {
+      page: page,
+      per_page: perPage
+    });
   }
 
   /**
@@ -38,6 +54,29 @@ export class Strava {
       key_by_type: keyByType
     });
   }
+}
+
+interface ActivityStats {
+  biggest_ride_distance: number;
+  biggest_climb_elevation_gain: number;
+  recent_ride_totals: ActivityTotal;
+  recent_run_totals: ActivityTotal;
+  recent_swim_totals: ActivityTotal;
+  ytd_ride_totals: ActivityTotal;
+  ytd_run_totals: ActivityTotal;
+  ytd_swim_totals: ActivityTotal;
+  all_ride_totals: ActivityTotal;
+  all_run_totals: ActivityTotal;
+  all_swim_totals: ActivityTotal;
+}
+
+interface ActivityTotal {
+  count: number;
+  distance: number;
+  moving_time: number;
+  elapsed_time: number;
+  elevation_gain: number;
+  achievement_count: number;
 }
 
 export enum StreamKeys {
@@ -70,17 +109,17 @@ interface StreamSet {
 
 interface Stream {
   type:
-  | 'time'
-  | 'distance'
-  | 'latlng'
-  | 'altitude'
-  | 'velocity_smooth'
-  | 'heartrate'
-  | 'cadence'
-  | 'watts'
-  | 'temp'
-  | 'moving'
-  | 'grade_smooth';
+    | 'time'
+    | 'distance'
+    | 'latlng'
+    | 'altitude'
+    | 'velocity_smooth'
+    | 'heartrate'
+    | 'cadence'
+    | 'watts'
+    | 'temp'
+    | 'moving'
+    | 'grade_smooth';
   original_size: number;
   resolution: 'low' | 'medium' | 'high';
   series_type: 'distance' | 'time';
