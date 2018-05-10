c022774e28c17778a27dd4f1ba025f82c9400c20
amorgulis <>
2018-05-10 16:32:46 -0700

    Fixed method comment.

--- src/api.ts
+++ src/api.ts
@@ -38,11 +38,8 @@ export class Strava {
   }
 
   /**
-   * Returns the the authenticated athlete's heart rate and power zones.
-   * @param id The identifier of the athlete.
-   * @param page Page number.
-   * @param perPage Number of items per page. Defaults to 30.
-   * @returns Returns the the authenticated athlete's heart rate and power zones.
+   * Returns the authenticated athlete's heart rate and power zones.
+   * @returns Returns the authenticated athlete's heart rate and power zones.
    */
   async getLoggedInAthleteZones() {
     const path = 'athlete/zones';
